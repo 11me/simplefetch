29f151c4fcfa8baca51d5fc1e2c907308a02de2d
lime <lime>
2022-01-01 17:12:50 +0300

    Add post method

--- simplefetch.go
+++ simplefetch.go
@@ -1,15 +1,30 @@
 package simplefetch
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"net/http"
 	"net/url"
 )
+
+var headers http.Header
+var client http.Client
+
+type Data map[string]interface{}
 
 type Options struct {
 	URL     string
 	Method  string
 	Headers http.Header
+	Data    Data
 }
+
+type bytesReadCloser struct {
+	io.Reader
+}
+
+func (brc bytesReadCloser) Close() error { return nil }
 
 func Get(o Options) (*http.Response, error) {
 	url, err := url.Parse(o.URL)
@@ -17,9 +32,8 @@ func Get(o Options) (*http.Response, error) {
 		return nil, err
 	}
 
-	client := &http.Client{}
+	client = http.Client{}
 
-	var headers http.Header
 	if o.Headers == nil {
 		headers = map[string][]string{
 			"User-Agent": {"simple-fetch"},
@@ -34,6 +48,40 @@ func Get(o Options) (*http.Response, error) {
 
 	res, err := client.Do(req)
 
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func Post(o Options) (*http.Response, error) {
+	client = http.Client{}
+
+	url, err := url.Parse(o.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	if o.Headers == nil {
+		headers = map[string][]string{
+			"User-Agent": {"simple-fetch"},
+		}
+	}
+
+	json_data, err := json.Marshal(o.Data)
+	if err != nil {
+		return nil, err
+	}
+
+	req := &http.Request{
+		URL:    url,
+		Method: http.MethodPost,
+		Header: headers,
+		Body:   bytesReadCloser{bytes.NewBuffer(json_data)},
+	}
+
+	res, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
--- simplefetch_test.go
+++ simplefetch_test.go
@@ -1,7 +1,9 @@
 package simplefetch_test
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,13 +13,13 @@ import (
 
 func TestFetch(t *testing.T) {
 
-	// create a local listening server
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(200)
-		fmt.Fprintf(w, "Test data")
-	}))
+	t.Run("GET method", func(t *testing.T) {
+		// create a local listening server
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(200)
+			fmt.Fprintf(w, "Test data")
+		}))
 
-	t.Run("Get method", func(t *testing.T) {
 		res, err := fetch.Get(fetch.Options{
 			URL: server.URL,
 		})
@@ -28,7 +30,39 @@ func TestFetch(t *testing.T) {
 		if res.StatusCode != 200 {
 			t.Errorf("Expected status code 200, but got %v", res.StatusCode)
 		}
+		defer server.Close()
 	})
 
-	defer server.Close()
+	t.Run("POST method", func(t *testing.T) {
+		dataMap := make(map[string]interface{})
+		// create a local listening server
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(200)
+
+			data, _ := io.ReadAll(r.Body)
+			json.Unmarshal(data, &dataMap)
+
+			if dataMap["id"] != float64(1) {
+				t.Errorf("Expected id 1, but got %d", dataMap["id"])
+			}
+
+			if dataMap["name"] != "lime" {
+				t.Errorf("Expected name lime, but got %s", dataMap["name"])
+			}
+
+			fmt.Fprintf(w, "Test data")
+		}))
+
+		res, err := fetch.Post(fetch.Options{
+			URL: server.URL,
+			Data: fetch.Data{
+				"id":   1,
+				"name": "lime",
+			},
+		})
+		if res.StatusCode != 200 {
+			t.Error(err)
+		}
+		defer server.Close()
+	})
 }
